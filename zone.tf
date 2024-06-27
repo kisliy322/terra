ae4bfc0fb0933c1011bd1a7e0e4074745dbf6356
kisliy322 <67372369+kisliy322@users.noreply.github.com>
2024-06-27 11:50:26 +0300

    Create zone.tf

---
+++ zone.tf
@@ -0,0 +1,23 @@
+variable "zone" {                                # Используем переменную для передачи в конфиг инфраструктуры
+  description = "Use specific availability zone" # Опционально описание переменной
+  type        = string                           # Опционально тип переменной
+  default     = "ru-central1-a"                  # Опционально значение по умолчанию для переменной
+}
+
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.60.0" # Фиксируем версию провайдера
+    }
+  }
+}
+
+# Документация к провайдеру тут https://registry.terraform.io/providers/yandex-cloud/yandex/latest/docs#configuration-reference
+# Настраиваем the Yandex.Cloud provider
+provider "yandex" {
+  service_account_key_file = file("~/.sa.json")
+  cloud_id                 = "MY_UNIQUE_ID"
+  folder_id                = "SKILLBOX_PROJECT"
+  zone                     = var.zone # зона, которая будет использована по умолчанию
+}
